e4e1242f17185e1b238c0730ed614acdef6fa1db
lena <113795106+liquidbeehive@users.noreply.github.com>
2024-03-15 11:14:32 -0400

    yay

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -11,18 +12,31 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const increment = (amount) => setCartPrice(cartTotal+amount)
+  const[cartTotal, setCartPrice] = useState(0)
+
+  const addToCartList = (foodItem) => setCartList([...cartContent, foodItem])
+  const[cartContent, setCartList] = useState([])
 
   return (
     <div className="App">
+      <div>
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
+        <BakeryItem  name = {item.name} price = {item.price} image = {item.image} increment={increment} addToCartList={addToCartList}/>
+      ))}</div>
 
       <div>
         <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        {cartContent.map((item, index) => ( 
+        <p> {item}</p> 
+      ))}
+
+
+        <p>
+        Your cart total is ${cartTotal}
+        </p>
       </div>
     </div>
   );
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,29 @@
 // TODO: create a component that displays a single bakery item
+
+export default function BakeryItem(prop) {
+    return (
+      <p>
+        <img src= {prop.image} height="100" alt = {prop.name}></img>
+        <p>
+        {prop.name}
+        </p>
+        <p>
+        {prop.price}
+        </p>
+        <button onClick = {() => buttonClickFunc(prop)} > buy me! </button>
+      </p>
+    
+    );
+  }
+  function buttonClickFunc(prop) {
+    prop.increment(prop.price)
+    prop.addToCartList(prop.name)
+  }
+
+// function Button(prop){
+// return (
+//   <button onClick = {prop.increment()}>
+//     {"buy me"}
+// </button>
+// )
+// }
